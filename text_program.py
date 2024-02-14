4ed762134d0bbed23a9fa4784b131aa84a63fd70
Simen <simeeid@stud.ntnu.no>
2024-02-14 15:37:57 +0100

    Added wack scrolling
    Added scrolling that need updating
    
    Issue: #23

--- text_program.py
+++ text_program.py
@@ -1,16 +1,20 @@
+import time
 import customtkinter as ctk
 from tkinter import filedialog, END, Checkbutton, IntVar
 from tkinter import Tk
 
 def load_file():
-    file_path = filedialog.askopenfilename(filetypes=[('Text Files', '*.txt')])
     text_widget.configure(state='normal')
+    # empty the text widget
+    # text_widget.delete('0.0', END)
+    file_path = filedialog.askopenfilename(filetypes=[('Text Files', '*.txt')])
     if file_path:
         with open(file_path, 'r') as file:
             content = file.read()
             text_widget.delete('0.0', END)
-            text_widget.insert(END, content)
-    text_widget.configure(state="disable")
+            text_widget.insert(END, content)  # apply the correct color scheme after loading the file
+    text_widget.configure(state='disable')
+    auto_scroll(text_widget, 0)
 
 def toggle_dark_mode():
     if dark_mode.get():
@@ -24,6 +28,33 @@ def toggle_dark_mode():
         ctk.set_appearance_mode('light')
         dark_mode_switch.configure(bg='white', fg='black', selectcolor='white')
 
+def auto_scroll(textbox, x):
+    """Scrolls the textbox at a constant speed."""
+    textbox.yview_moveto(x)  # Scrolls to the end of the textbox
+
+    num_lines = textbox.get('1.0', END).count('\n')
+    # find the width of the window
+    window_width = textbox.winfo_width()
+
+    x = x + 0.00004 / num_lines * window_width
+    textbox.after(10, lambda: auto_scroll(textbox, x))
+
+
+    # find the height of the text document or the number of lines, then update x proportionally
+    #x = 1 - (textbox.yview()[1] - textbox.yview()[0])  # 1 - (bottom - top)
+    # get the height of the text widget
+
+    # find the number of line changes in the text widget
+    # print(num_line_breaks)
+
+    # textbox.after(10, lambda: auto_scroll(textbox, x))  # Calls itself every 100ms
+
+    # textbox.yview_scroll(1, 'units')  # scroll 10% of the height of the text widget
+    # if x%10 == 0:
+    #     textbox.after(1000, lambda: auto_scroll(textbox, x+1))
+    # else:
+    #     textbox.after(100, lambda: auto_scroll(textbox, x+1))
+
 root = Tk()
 root.geometry('500x500')
 root.minsize(500, 500)
@@ -34,6 +65,13 @@ text_widget.configure(wrap='word', state='disable')
 
 frame = ctk.CTkFrame(root, height=32, fg_color='red')
 frame.place(relx=0.05, rely=0.925, relwidth=0.9)
+
+# upper_widget = ctk.CTkFrame(root, fg_color='blue')
+# upper_widget.place(relx=0.0, rely=0.0, relheight=0.25, relwidth=1)
+
+# make lower transparent, alpha=0.5 is not a valid argument
+# lower_widget = ctk.CTkFrame(root)
+# lower_widget.place(relx=0.0, rely=0.75, relheight=0.25, relwidth=1)
 
 load_button = ctk.CTkButton(frame, width=150, text="Load .txt file", command=load_file)
 load_button.grid(row=0, column=0, sticky=ctk.N+ctk.S)
