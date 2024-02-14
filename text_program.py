2dc244d4745b32cc2bfef913470ab5a1aae0fece
WilliamVoster <46726271+WilliamVoster@users.noreply.github.com>
2024-02-14 12:50:57 +0100

    Set minimum window size
    fixed textbox to be non-editable and cleaned up some comments
    
    Issue: #23

--- text_program.py
+++ text_program.py
@@ -9,7 +9,8 @@ def load_file():
         with open(file_path, 'r') as file:
             content = file.read()
             text_widget.delete('0.0', END)
-            text_widget.insert(END, content)  # apply the correct color scheme after loading the file
+            text_widget.insert(END, content)
+    text_widget.configure(state="disable")
 
 def toggle_dark_mode():
     if dark_mode.get():
@@ -24,17 +25,18 @@ def toggle_dark_mode():
         dark_mode_switch.configure(bg='white', fg='black', selectcolor='white')
 
 root = Tk()
-root.geometry('300x300')
+root.geometry('500x500')
+root.minsize(500, 500)
 
 text_widget = ctk.CTkTextbox(root)
-text_widget.place(relx=0.05, rely=0.025, relwidth=0.9, relheight=0.9)  # added relx, rely for left and right margins
-text_widget.configure(wrap='word', state='disable')  # wrap around whole words
+text_widget.place(relx=0.05, rely=0.025, relwidth=0.9, relheight=0.9)
+text_widget.configure(wrap='word', state='disable')
 
 frame = ctk.CTkFrame(root, height=32, fg_color='red')
 frame.place(relx=0.05, rely=0.925, relwidth=0.9)
 
 load_button = ctk.CTkButton(frame, width=150, text="Load .txt file", command=load_file)
-load_button.grid(row=0, column=0, sticky=ctk.N+ctk.S)  # place the button below the text widget
+load_button.grid(row=0, column=0, sticky=ctk.N+ctk.S)
 
 dark_mode = IntVar()
 dark_mode_switch = Checkbutton(frame, text="Dark Mode", variable=dark_mode, command=toggle_dark_mode)
