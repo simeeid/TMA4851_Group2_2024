4184784ccf47ea64eea4cecd66729cb1ba89ccaf
Albert <albert.bayazidi@gmail.com>
2024-02-19 20:53:00 +0100

    added classes and game
    made a small target game. Moved Face into its own class. Tracker was moved into its own class (the model, the window, and the cam). A bug was introduced, sometimes the target is hit when the end effector is not near it. Noticed that when reading from landmark file, this is not done from root folder (should be change in other files as well
    
    related to issue #35

---
+++ classes/face.py
@@ -0,0 +1,32 @@
+import cv2
+import dlib
+import numpy as np
+
+
+class Face:
+    def __init__(self,frame,face,gray,predictor):
+        x, y, w, h = face.left(), face.top(), face.width(), face.height()
+        
+        self.center = (int(x+w/2), int(y+h/2))
+        self.landmarks = predictor(gray, face)
+        self.draw_landmarks(frame)
+        self.vector, self.nose_top = self.get_face_data()
+
+        cv2.circle(frame, (self.center), 10, (0, 0, 255), -1)  
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)            
+
+
+    def draw_landmarks(self, frame):
+        for n in range(0, 68):
+            x = self.landmarks.part(n).x
+            y = self.landmarks.part(n).y
+            cv2.circle(frame, (x, y), 1, (0, 255, 0), -1)
+    
+    def get_face_data(self):
+        # Calculate forward vector from ear to nose top
+        left_ear = np.array([self.landmarks.part(0).x, self.landmarks.part(0).y])
+        right_ear = np.array([self.landmarks.part(16).x, self.landmarks.part(16).y])
+        ear_midpoint = (left_ear + right_ear) / 2
+        nose_top = np.array([self.landmarks.part(27).x, self.landmarks.part(27).y])
+        forward_vector = nose_top - ear_midpoint
+        return forward_vector, nose_top
---
+++ classes/tracker.py
@@ -0,0 +1,95 @@
+import cv2
+import dlib
+import numpy as np
+from face import Face
+from game import Game
+
+
+class Tracker:
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+
+        if not self.cap.isOpened():
+            raise IOError("Cannot open webcam")
+        # Modelling tools
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor('classes/shape_predictor_68_face_landmarks.dat')
+
+        # Get screen dimensions
+        self.screen_width = int(self.cap.get(3))
+        self.screen_height = int(self.cap.get(4))
+    
+        self.vec_sacle = 5
+        self.game = False
+
+        self.x = np.linspace(-1,1,1000)
+        self.y = self.x
+        self.epsilon = 5                    #tolerance for reaching target
+
+
+    def draw_vector(self, frame, start_point, direction_vector, scale=5, color=(0, 255, 0), thickness=2):
+        # Draw a vector (line) on the frame to indicate direction
+        end_point = (int(start_point[0] + direction_vector[0] * scale), int(start_point[1] + direction_vector[1] * scale))
+        cv2.line(frame, start_point, end_point, color, thickness)
+
+    def start_video(self):
+        # Main tracking loop
+        
+        target_cords = 0        #testing
+        endeffector = 0         #testing
+        while True:
+            ret, frame = self.cap.read()
+            if not ret:
+                break
+
+            frame = cv2.flip(frame, 1)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)          # grayscale image
+            faces = self.detector(gray, 0)
+            
+            for face in faces:
+                if face is not None:
+                    face_obj = Face(frame,face,gray,self.predictor)
+                    forward_vector, nose_top, center = face_obj.vector, face_obj.nose_top, face_obj.center
+                    self.draw_vector(frame, np.array(nose_top), forward_vector, scale=self.vec_sacle, color=(255, 0, 0), thickness=2)
+
+                    # gjør om til funksjoner
+                    red_vec = np.array((center[0]-nose_top[0],center[1]-nose_top[1]))                   
+                    endeffector = np.array((center[0]+red_vec[0]+self.vec_sacle*forward_vector[0],
+                                            center[1]+red_vec[1]+self.vec_sacle*forward_vector[1]),dtype=int)
+                else :
+                    print("No face detected")
+                               
+
+            match cv2.waitKey(1):   # Exit on ESC
+                    case 27:
+                        break
+                    case 103:
+                        print("target game started")
+                        cords = np.random.rand(1,2)
+                        target_cords = np.array((cords[0][0]*self.screen_width, cords[0][1]*self.screen_height),dtype=int)
+                        self.game = True
+                        
+            if self.game:
+                target_state = ((self.x-target_cords[0])**2+(self.y-target_cords[1])**2 
+                                - self.epsilon**2 < endeffector[0]**2+endeffector[1]**2).all() # makes circle around 
+                                                                #target (becomes area of target) (bug red dot seems to also count?)
+                if target_state:
+                    cv2.circle(frame, (target_cords[0],target_cords[1]), 10, (255, 255, 0), -1)
+                    new_cords = np.random.rand(1,2)
+                    target_cords = np.array((new_cords[0][0]*self.screen_width, new_cords[0][1]*self.screen_height),dtype=int)
+                    cords  = new_cords
+                else:
+                    cv2.circle(frame, (target_cords[0],target_cords[1]), 10, (255, 255, 0), -1)
+            else:
+                pass
+            
+
+
+            cv2.imshow('Frame', frame)
+        # Release and close
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    Tracker().start_video()
